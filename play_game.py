bf83e364e8dfb1e2e6b06832a550294cc6d45045
Derfador1 <Derfador1@gmail.com>
2016-03-10 12:23:50 -0500

    made sure to work in code for monster atk or hero atk

--- play_game.py
+++ play_game.py
@@ -33,30 +33,33 @@ def main():
 		
 		print(monster)
 		
-		print('Monster:', m_initiative)
-		print('Hero:', h_initiative)
+		#print('Monster:', m_initiative)
+		#print('Hero:', h_initiative)
 		
 		if m_initiative >= h_initiative:
-			print('Monster wins initiative, it attacks')
-					
+			print('\nMonster wins initiative, it attacks')
 			dict_m = monster.roll_atk()
 			set_m = set(dict_m.values())
 			max_m = max(set_m)
-			print(max_m)
+			print('Monster rolls:', max_m)
 			
 			dict_h = hero1.roll_atk()
 			set_h = set(dict_h.values())
 			max_h = max(set_h)
-			print(max_h)
+			print('Hero rolls:', max_h)
 			
 			if max_m > max_h:
-				print('Hero gets hit')
+				print('Hero looses roll')
 				monster.attack(hero1)
-			
-			
+			else:
+				print('Hero wins roll')
+		else:
+			print('Hero wins initiative')
 
+			
 		while True:
 			print()
+			
 			if hero1.char_death() == 'Dead':
 				print('\nYour Hero has died\n')
 				exit(1)
@@ -73,15 +76,50 @@ def main():
 			for entry in options:
 				print(entry, menu[entry])
 
-			selection = input("Choose an option: ")
+			selection = input("\nChoose an option: ")
 			
 			if selection == '1':
 				print('1')
 			elif selection == '2':
 				if monster.char_death() == 'Dead':
-					print('\nYou have killed the monster, move to next location\n')
+					print('\nYou have killed the monster, move to next location')
 				else:
-					hero1.attack(monster)
+					dict_h = hero1.roll_atk()
+					set_h = set(dict_h.values())
+					max_h = max(set_h)
+					print('Hero rolls:', max_h)
+					
+					dict_m = monster.roll_atk()
+					set_m = set(dict_m.values())
+					max_m = max(set_m)
+					print('Monster rolls:', max_m)
+					
+					if max_h >= max_m:
+						print('Monster looses roll')
+						hero1.attack(monster)
+					else:
+						print('Monster wins roll')
+					
+					if monster.char_death() != 'Dead':
+						print('\nMonster attacks back\n')
+						dict_m = monster.roll_atk()
+						set_m = set(dict_m.values())
+						max_m = max(set_m)
+						print('Monster rolls:', max_m)
+						
+						dict_h = hero1.roll_atk()
+						set_h = set(dict_h.values())
+						max_h = max(set_h)
+						print('Hero rolls:', max_h)
+						
+						if max_m > max_h:
+							print('Hero looses roll')
+							monster.attack(hero1)
+						else:
+							print('Hero wins roll')
+					else:
+						print('You just killed the monster')
+					
 			elif selection == '3':
 				if monster.char_death() == 'Dead':
 					num += 1
@@ -89,9 +127,9 @@ def main():
 				else:
 					print('You still need fight the monster before you!')
 			elif selection == '4':
-				print('\nRemaining hero health:', hero1._health, '\n')
+				print('\nRemaining hero health:', hero1._health)
 			elif selection == '5':
-				print('\nRemaining monster health:', monster._health, '\n')
+				print('\nRemaining monster health:', monster._health)
 			elif selection == '6':
 				print('Quitting...')
 				exit(1)
