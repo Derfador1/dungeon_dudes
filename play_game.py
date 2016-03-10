208bd678b0030cf394e44c59fd37cdfc787990aa
Derfador1 <Derfador1@gmail.com>
2016-03-10 09:24:18 -0500

    added in roll and check for initiative

--- play_game.py
+++ play_game.py
@@ -11,26 +11,36 @@ location_num = 10
 def main():
 	print('Welcome to Dungeon Dudes\n')
 	
-	hero_name = ['Ash', 'Naruto', 'Ichigo', 'One Punch Man']
-	name = random.randint(0,3)
+	#start
+	
+	hero_name = ['Ash', 'Naruto', 'Ichigo', 'One Punch Man', 'Scooby Doo']
+	name = random.randint(0,4)
 	
 	hero1 = d.Hero(hero_name[name], 10, 10)
 	print(hero1)
 	
-	num = 1
+	num = 0
 	
 	while num < location_num:
-	
+		#numb_mon = random.randint(1,3)
+		room = d.Location()
+		monster = d.Monster(random.randint(1,3))
 		
-		monster = d.Monster(random.randint(1,3))	
-	
 		print(monster)
-		
-		name_list = ['Doom', 'Poop', 'Apples']
-		rand_name = random.randint(0,2)
-		
-		room = d.Location(name_list[rand_name])
 		print(room)
+		
+		m_initiative = monster.initiative()
+		h_initiative = hero1.initiative()
+		
+		print('Monster:', m_initiative)
+		print('Hero:', h_initiative)
+		
+		
+		if m_initiative > h_initiative:
+			print('Monster wins initiative, it attacks')
+			#rolls attack
+			hero1.defend()
+
 		
 		menu = {'1':'List items in loot bag', '2':'Attack monster', '3':'Move to next location', '4':'List remaining health', '5':'List health for monster'}
 		
@@ -40,7 +50,6 @@ def main():
 			print(entry, menu[entry])
 
 		selection = input("Choose an option: ")
-
 		
 		if selection == '1':
 			print('1')
